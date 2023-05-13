44ec4dac46c82a83f6ee5d534b90e35697b366a7
shwetapise9292 <shwetapise9292@gmail.com>
2023-05-13 23:29:08 +0530

    Commited and Pushed...

---
+++ 01_variable_assign.js
@@ -0,0 +1,29 @@
+
+
+
+
+var myFullName;
+console.log(myFullName);
+myFullName = "Shweta Pise";
+console.log(myFullName);
+myFullName = "Shweta Amar Pise";
+console.log(myFullName);
+
+var myLove;
+myLove = "I love only JS";
+console.log(myLove);
+
+var firstName = "Shweta";
+console.log(firstName);
+var lastName = "Pise";
+console.log(lastName);
+var age = "25";
+console.log(age);
+var address = "A/P - Islampur";
+console.log(address);
+var pincode = "415409";
+console.log(pincode);
+var gender = "Female";
+console.log(gender);
+var isMarried = "Yes";
+console.log(isMarried);
---
+++ hello.js
@@ -0,0 +1,21 @@
+
+
+
+
+console.log("Shweta Pise...");
+console.log("A/P- Islampur");
+
+var firstName = "Shweta";
+console.log(firstName);
+
+var age; // variable declaration
+age = "22"; // variable Initialization
+console.log(age);
+
+var holder;  // variable declaration
+console.log(holder); // undefined
+holder = "My Spec" // variable initialization
+console.log(holder);
+
+holder = "Sachin Spec"; // variable update
+console.log(holder);
