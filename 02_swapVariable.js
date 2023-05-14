28e22d110a3fcee87dad9701804b3624b2af462d
shwetapise9292 <shwetapise9292@gmail.com>
2023-05-14 23:28:38 +0530

    Performed swaping
    Performed swaping

---
+++ 02_swapVariable.js
@@ -0,0 +1,10 @@
+
+
+var num1 = 100;
+var num2 = 200;
+
+console.log("Before swap..", "num1:", num1, "num2:", num2);
+var temp = num1;
+num1 = num2;
+num2 = temp;
+console.log("After swap..", "num1:", num1, "num2:", num2);
