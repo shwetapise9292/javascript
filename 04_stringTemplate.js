e9c7548ac0e6df0b1aa7bdfd2642bca24d822828
shwetapise9292 <shwetapise9292@gmail.com>
2023-05-19 07:12:43 +0530

    String templates
    String templates

---
+++ 04_stringTemplate.js
@@ -0,0 +1,48 @@
+var greet = "Good Morning mate, How are you";
+
+var resultSplit = greet.split(" ");
+console.log(resultSplit);
+console.log("Total number of words: ", resultSplit.length);
+
+var resultSplit = greet.split(",");
+console.log(resultSplit);
+console.log("Total number parts: ", resultSplit.length);
+
+var resultSplit = greet.split("");
+console.log(resultSplit);
+console.log("Total number of words: ", resultSplit.length);
+
+//Part 2
+
+var greet = "Good Morning mate, How are you";
+var resultSplit = greet.split(" ");
+console.log(resultSplit);
+
+//console.log("After slipt words are:",resultSplit, "Total number of words: ",resultSplit.length);
+
+console.log(
+  `After slipt words are ${resultSplit} Total number of words ${resultSplit.length} `
+);
+console.log(`Given string is: ${greet}`);
+
+var myName = "Elon Musk";
+var result = myName.startsWith("E");
+console.log(` Is ${myName} starts with character E: ${result}`); 
+
+var result = myName.startsWith("Elon");
+console.log(` Is ${myName} starts with character "Elon" : ${result}`);
+
+var result = myName.startsWith("elon");
+console.log(` Is ${myName} starts with character "elon" : ${result}`);
+
+var myName = "Elon Musk";
+var result = myName.endsWith("k");
+console.log(` Is ${myName} ends with character "k" : ${result}`);
+
+var myName = "Elon Musk";
+var result = myName.endsWith("Musk");
+console.log(` Is ${myName} ends with character "Musk" : ${result}`);
+
+var myName = "Elon Musk";
+var result = myName.endsWith("b");
+console.log(` Is ${myName} ends with character "b" : ${result}`);
