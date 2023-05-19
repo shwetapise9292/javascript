46f149987c90b907e7879b43335e7ac327078d93
shwetapise9292 <shwetapise9292@gmail.com>
2023-05-19 07:08:59 +0530

    swapped variables
    swapped variables

---
+++ 02_swapvariablesAssign.js
@@ -0,0 +1,25 @@
+
+
+
+
+var sweety = "Sweety";
+var cutie = "Cutie";
+
+console.log("Before swap:", sweety, cutie);
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log("After swap:", sweety, cutie);
+
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+
+console.log("--------------------------------------------")
+
+console.log("Before swap..", "num1:", num1, "num2:", num2, "num3:", num3);
+var temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log("After swap..", "num1:", num1, "num2:", num2, "num3:", num3);
