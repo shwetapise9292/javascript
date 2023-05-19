f57079fb5969cd2109f588db488df30c893894db
shwetapise9292 <shwetapise9292@gmail.com>
2023-05-19 07:11:06 +0530

    Used typeof operator

---
+++ 03_typeOf.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+var bankName;
+var typeOfBankName = typeof bankName;
+console.log("Bank Name:", bankName,",", "Type:",typeOfBankName);
+
+bankName = "SBI Bank";
+console.log("Bank Name:", bankName,",", "Type:",typeOfBankName);
+console.log("----------------------------------------------");
+
+var marks = "90%";
+var typeOfMarks = typeof marks;
+console.log("Type of Variable marks:", typeOfMarks,",", "Marks:",marks);
+console.log("----------------------------------------------");
+
+var isWorking;
+console.log("Is Working before Initialization:",isWorking);
+var isWorking = true;
+console.log("Is Working after Initialization:", isWorking);
+console.log("----------------------------------------------");
+
+var totalCount = "Hundred and Seven";
+var typeofTotalCount = typeof totalCount;
+console.log("Total count:",totalCount, "Type:",typeofTotalCount);
+totalCount = 107;
+typeofTotalCount = typeof totalCount;
+console.log("Total count:",totalCount, "Type:",typeofTotalCount);
+
+
+
