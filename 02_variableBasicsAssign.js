b5471fdc18c86b24e4b44db1ec8104f1c11da741
shwetapise9292 <shwetapise9292@gmail.com>
2023-05-19 07:10:11 +0530

    Basic variables

---
+++ 02_variableBasicsAssign.js
@@ -0,0 +1,15 @@
+
+
+
+
+var empNo = 1001;
+var empName = "Aradhya Patil";
+var knowHTML = true; 
+
+var typeofEmpNo = typeof empNo;
+var typeofEmpName = typeof empName;
+var typeofKnowHTML = typeof knowHTML; 
+
+console.log("Employee No.:", empNo,",", "Type:", typeofEmpNo);
+console.log("Employee Name:", empName,",", "Type:", typeofEmpName);
+console.log("Is Know HTML?:", knowHTML,",", "Type:", typeofKnowHTML);
