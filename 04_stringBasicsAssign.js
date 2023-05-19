d22237622618e39b7f5a8788e490547020f6d7b6
shwetapise9292 <shwetapise9292@gmail.com>
2023-05-19 07:12:12 +0530

    String basics
    String basics

---
+++ 04_stringBasicsAssign.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+function stringBasics()
+{
+    var dreamCompany = "Infosys";
+    console.log("My dream company:",dreamCompany);
+    console.log("----------------------------------------------------")
+
+    var hobby1 = "Reading";
+    var hobby2 = "Singing";
+    var hobby3 = "Listening songs";
+    console.log("My hobbies:", hobby1,",",hobby2,",",hobby3);
+    console.log(" ");
+
+    var hobby1Length = hobby1.length;
+    console.log("Lngth of Reading:", hobby1Length);
+
+    var hobby2Length = hobby2.length;
+    console.log("Length of Singing:", hobby2Length);
+
+    var hobby3Length = hobby3.length;
+    console.log("Length of Listening songs:", hobby3Length);
+    
+}
+stringBasics();
