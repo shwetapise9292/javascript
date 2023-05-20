d4c48ab3f2de00ba97f3e8c1b75aee6495a8c868
shwetapise9292 <shwetapise9292@gmail.com>
2023-05-20 19:54:16 +0530

    sum of length of hobbies calculated
    sum of length of hobbies calculated

--- 04_stringBasicsAssign.js
+++ 04_stringBasicsAssign.js
@@ -16,13 +16,16 @@ function stringBasics()
     console.log(" ");
 
     var hobby1Length = hobby1.length;
-    console.log("Lngth of Reading:", hobby1Length);
+    // console.log("Lngth of Reading:", hobby1Length);
 
     var hobby2Length = hobby2.length;
-    console.log("Length of Singing:", hobby2Length);
+    // console.log("Length of Singing:", hobby2Length);
 
     var hobby3Length = hobby3.length;
-    console.log("Length of Listening songs:", hobby3Length);
+    // console.log("Length of Listening songs:", hobby3Length);
+
+    var sumHobbyLength = hobby1Length + hobby2Length + hobby3Length;
+    console.log("Sum of length of hobbies:", sumHobbyLength);
     
 }
 stringBasics();
