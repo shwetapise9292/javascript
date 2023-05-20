7864b3d5123a15d1eb7b09a43c4b5498f6dec1ad
shwetapise9292 <shwetapise9292@gmail.com>
2023-05-20 19:47:57 +0530

    Added difference between null and undefined
    Added difference between null and undefined

--- 02_variableBasicsAssign.js
+++ 02_variableBasicsAssign.js
@@ -6,10 +6,17 @@ var empNo = 1001;
 var empName = "Aradhya Patil";
 var knowHTML = true; 
 
+
 var typeofEmpNo = typeof empNo;
 var typeofEmpName = typeof empName;
 var typeofKnowHTML = typeof knowHTML; 
 
+
 console.log("Employee No.:", empNo,",", "Type:", typeofEmpNo);
 console.log("Employee Name:", empName,",", "Type:", typeofEmpName);
 console.log("Is Know HTML?:", knowHTML,",", "Type:", typeofKnowHTML);
+
+console.log(" ");
+
+var difference = "Null - It is an assignment value. It can be assigned to a variable which indicates that a variable does not point any object. Undefined - It is not an assignment value. It means a variable has been declared but has not yet been assigned a value.";
+console.log("Difference between null vs undefined:", difference);
