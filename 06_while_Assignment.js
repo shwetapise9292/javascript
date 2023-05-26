517b5a068aa37ba1e9220266a5b8ec6c5fa04579
shwetapise9292 <shwetapise9292@gmail.com>
2023-05-26 12:30:49 +0530

    while loop assignment
    while loop assignment

---
+++ 06_while_Assignment.js
@@ -0,0 +1,72 @@
+
+
+
+
+//WAP to print numbers from 5 to 15 by incrementing 1
+console.log("1. Numbers from 5 to 15 by incrementing 1:");
+let i= 5 ;
+while (i<=15) {
+    console.log(i);
+    i++;    
+}
+
+//WAP to print numbers from 50 to 40 by decrementing 1
+console.log("2. Numbers from 50 to 40 by incrementing 1:");
+let j = 50 ;
+while (j>=40) {
+    console.log(j);
+    j--;    
+}
+
+//WAP to find first 15 odd numbers
+console.log("3. First 15 odd numbers:");
+let p=0;  
+while (p<=30) {
+   if (p%2!=0) {
+      console.log(p);
+   }
+   p++; 
+}
+
+//WAP to find first 10 even numbers
+console.log("4. First 10 even numbers:");
+let q=0;  
+while (q<20) {
+   if (q%2==0) {
+      console.log(q);
+   }
+   q++; 
+}
+
+//WAP to print table of 5 like -> 5 10 15 20 25 ... 50
+console.log("5. table of 5:");
+let a=5;  
+while (a<=50) {
+   if (a%5==0) {
+      console.log(a);
+   }
+   a++; 
+}
+
+//WAP to print table of 10 like -> 10 20 30 40 ... 100
+console.log("6. table of 10:");
+let b=10;  
+while (b<=100) {
+   if (b%10==0) {
+      console.log(b);
+   }
+   b++; 
+}
+
+//WAP to print table of 10 in reverse order like -> 10 20 30 40 ... 100
+console.log("7. table of 10 in reverse order:");
+let s=100;  
+while (s>=10) {
+   if (s%10==0) {
+      console.log(s);
+   }
+   s--; 
+}
+
+
+
